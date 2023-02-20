11c11543f5b639ec13d60a4709f4b158c00a6466
杨凯 <906580223@qq.com>
2023-02-20 16:28:37 +0800

    fix: fix build error

--- src/main.js
+++ src/main.js
@@ -41,20 +41,6 @@ Vue.use(Pagination)
 Vue.use(FormItem)
 Vue.use(Link)
 Vue.use(Loading)
-
-/** Pagination
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-if (process.env.NODE_ENV === 'production') {
-  const { mockXHR } = require('../mock')
-  mockXHR()
-}
-
 Vue.config.productionTip = false
 
 new Vue({
